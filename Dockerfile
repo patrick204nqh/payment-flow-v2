6194e0eeb0b68d05e6ec19696768189a8658dbe6
Nguyen Quang Huy <patrick204nqh@gmail.com>
2022-06-02 21:21:09 +0700

    Init template NarHakobyan/awesome-nest-boilerplate

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,30 @@
-FROM node:16.15.0
-
-RUN yarn global add @nestjs/cli
-
-WORKDIR /var/www/app
-
+FROM node:lts AS dist
 COPY package.json yarn.lock ./
 
 RUN yarn install
 
-EXPOSE 3000
+COPY . ./
 
-CMD yarn run start
+RUN yarn build:prod
+
+FROM node:lts AS node_modules
+COPY package.json yarn.lock ./
+
+RUN yarn install --prod
+
+FROM node:lts
+
+ARG PORT=3000
+
+RUN mkdir -p /usr/src/app
+
+WORKDIR /usr/src/app
+
+COPY --from=dist dist /usr/src/app/dist
+COPY --from=node_modules node_modules /usr/src/app/node_modules
+
+COPY . /usr/src/app
+
+EXPOSE $PORT
+
+CMD [ "yarn", "start:prod" ]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,54 @@
+import './boilerplate.polyfill';
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { I18nJsonParser, I18nModule } from 'nestjs-i18n';
+import path from 'path';
+
+import { AllExceptionsFilter } from './filters/all-exceptions.filter';
+import { AuthModule } from './modules/auth/auth.module';
+import { HealthCheckerModule } from './modules/health-checker/health-checker.module';
+import { PostModule } from './modules/post/post.module';
+import { UserModule } from './modules/user/user.module';
+import { ApiConfigService } from './shared/services/api-config.service';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@mongodb:27017`),
     AuthModule,
-    UsersModule
+    UserModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [SharedModule],
+      useFactory: (configService: ApiConfigService) =>
+        configService.postgresConfig,
+      inject: [ApiConfigService],
+    }),
+    I18nModule.forRootAsync({
+      useFactory: (configService: ApiConfigService) => ({
+        fallbackLanguage: configService.fallbackLanguage,
+        parserOptions: {
+          path: path.join(__dirname, '/i18n/'),
+          watch: configService.isDevelopment,
+        },
+      }),
+      imports: [SharedModule],
+      parser: I18nJsonParser,
+      inject: [ApiConfigService],
+    }),
+    HealthCheckerModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class AppModule {}
